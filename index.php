6b1500bde59fbf95369c00e2b21e34f334f013d4
Edugon0 <eduardo.vic.goncalves@gmail.com>
2024-11-27 14:54:40 -0300

    coloque aqui o nome que desejar

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign In</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <header><h1>Sign In</h1></header>
+
+    <main>
+        <form action="" method="get">
+            <h2>Login</h2>
+
+            <label for="username or email">Username or email</label>
+            <input type="text" name="input_username" id="">
+
+            <label for="password">password</label>
+            <input type="password" name="input_password" id="">
+            <a href="">esqueceu a senha?</a>
+            <br>
+
+            <input type="checkbox" name="manterlogado" id="">
+            <label for="manterlogado">Manter logado a minha conta nesse site</label>
+
+            <input type="submit" value="Enter With Your Account">
+
+
+            
+        </form>
+        <button><a href="register.php">Register Your Account</a></button>
+    </main>
+    
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+    <header><h1>Sign Up</h1></header>
+
+
+    <main>
+        <form action="" method="get">
+            <label for="username">username</label>
+            <input type="text" name="input_username" id="">
+
+            <label for="email">E-mail</label>
+            <input type="email" name="input_email" id="">
+
+            <label for="password">password</label>
+            <input type="password" name="input_password" id="">
+
+            <label for="confirmPassword"> confirm Password</label>
+            <input type="password" name="input_confirmPassword" id="">
+
+            <input type="checkbox" name="" id="">
+            <label for="contrato">Aceito os termo de <a href="">compromisso</a></label>
+            <br>
+            <input type="checkbox" name="" id="" value="">
+            <label for="contrato">Aceito receber informaçõe no meu e-mail</label>
+
+            <input type="submit" value="Register your account">
+        </form>
+        <button><a href="index.php">Go back to Login</a></button>
+    </main>
+</body>
+</html>
